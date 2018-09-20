75b85e8b19cc5874df52c62e0dc35aeba86557fb
alanPeng <alan@dcbiomed.com>
2018-09-20 15:56:53 +0800

    add JobModel.cs

---
+++ prismXamarinApp/prismXamarinApp/Models/JobModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prismXamarinApp.Models
+{
+    public class JobModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ prismXamarinApp/prismXamarinApp/Services/IJobService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using prismXamarinApp.Models;
+
+namespace prismXamarinApp.Services
+{
+    public interface IJobService
+    {
+        Task<List<JobModel>> GetJob();
+    }
+}
