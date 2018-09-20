a7bc33e99098dbbb3d623adabef4ff813ab844f4
alanPeng <alan@dcbiomed.com>
2018-09-20 14:59:45 +0800

    add Exception class

---
+++ prismXamarinApp/prismXamarinApp/Exceptions/ApiException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+
+namespace prismXamarinApp.Exceptions
+{
+    public class ApiException : Exception
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool Connection { get; set; }
+
+        public ApiException(string message, HttpStatusCode statusCode)
+            : base(message)
+        {
+            StatusCode = statusCode;
+            Connection = true;
+        }
+
+        public ApiException(string message, bool connection, Exception inner)
+            : base(message, inner)
+        {
+            Connection = connection;
+            StatusCode = HttpStatusCode.ServiceUnavailable;
+        }
+        public class ApiExceptionError
+        {
+            public string Message { get; set; }
+        }
+    }
+}
